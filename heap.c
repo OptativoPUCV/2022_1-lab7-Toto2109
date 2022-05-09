874cc3fafda2a1afb36dd491051f4b3380dca5a2
Your Name <you@example.com>
2022-05-08 23:28:29 -0400

    new try

--- heap.c
+++ heap.c
@@ -35,8 +35,8 @@ void heap_push(Heap* pq, void* data, int priority){
       pq -> heapArray = (heapElem*) realloc (pq -> heapArray, pq -> capac * sizeof(heapElem));
    }
 
-   pq -> heapArray[pq -> size].data = data;
-	pq -> heapArray[pq -> size].priority = priority;
+   pq -> heapArray[pq -> size + 1].data = data;
+	pq -> heapArray[pq -> size + 1].priority = priority;
 
 
 }
